5c22dcea341d3f9efd0aaeafb36328963db91415
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2022-02-24 03:46:34 +0400

    added comments in 3rd homework in common folder

--- common/lesson_3/homework_3/include/threadpool.h
+++ common/lesson_3/homework_3/include/threadpool.h
@@ -12,10 +12,13 @@ typedef void* input_t;
 typedef void* output_t;
 typedef output_t (*work_t)(input_t);
 
+// Creates a new threadpool.
 threadpool_t* create_threadpool(size_t size);
 
+// Executes a new function pointer named work on the threadpool.
 void execute(threadpool_t* threadpool, work_t work);
 
+// Destroys the threadpool.
 void destroy_threadpool(threadpool_t* threadpool);
 
 #endif
