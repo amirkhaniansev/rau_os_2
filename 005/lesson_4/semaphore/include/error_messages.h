c165a7e7007971b691baebaa21b31f4568d1de9b
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2022-03-03 01:07:02 +0400

    added thread and semaphore examples in lesson 4 for group 005

---
+++ 005/lesson_4/semaphore/include/error_messages.h
@@ -0,0 +1,7 @@
+#ifndef __ERROR_MESSAGES_H__
+#define __ERROR_MESSAGES_H__
+
+#define ERROR_FORMAT "%s\n"
+#define ARGS_ERROR   "Please, provide the size of queue and the amounts of producers and consumers."
+
+#endif
---
+++ 005/lesson_4/thread/main.cc
@@ -0,0 +1,14 @@
+#include <queue>
+#include <iostream>
+
+#include <pthread.h>
+#include <unistd.h>
+
+typedef void (*work_t)(void*);
+
+
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
