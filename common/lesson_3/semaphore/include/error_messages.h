2568835eb4cd4e096221654f5f275cf2f5f543e9
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2022-03-09 23:48:22 +0400

    removed 3rd lesson from common

--- common/lesson_3/semaphore/include/error_messages.h
+++
@@ -1,7 +0,0 @@
-#ifndef __ERROR_MESSAGES_H__
-#define __ERROR_MESSAGES_H__
-
-#define ERROR_FORMAT "%s\n"
-#define ARGS_ERROR   "Please, provide the size of queue and the amounts of producers and consumers."
-
-#endif
